1ba8152eaffb529a7ac440fd2e62887304e9052d
Maria <msegber@gmail.com>
2024-10-06 11:33:26 +0200

    feat: group products by category

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,6 @@
 import ProductCard from './ProductCard';
 import Grid from '@mui/material/Grid2';
-import { Box, Stack, Skeleton, CardMedia } from '@mui/material';
+import { Box, Stack, Skeleton, CardMedia, Typography } from '@mui/material';
 import { Product } from '../types';
 
 type Props = {
@@ -20,6 +20,18 @@ function Home({
   decreaseQuantity,
   getProductQuantity,
 }: Props) {
+  const productsGroupedByCategory = products.reduce(
+    (group: { [key: string]: Product[] }, product) => {
+      const { category } = product;
+      if (!group[category]) {
+        group[category] = [];
+      }
+      group[category].push(product);
+      return group;
+    },
+    {}
+  );
+
   return (
     <Stack
       sx={{
@@ -35,35 +47,44 @@ function Home({
       {/* <Box sx={{ width: '14rem', mb: 2 }}>
         <CardMedia component='img' src='./img/logo.svg' alt='logo' /> TODO: Add image
       </Box> */}
-      <Grid
-        container
-        alignItems='center'
-        justifyContent='center'
-        p={5}
-        spacing={2}
-      >
-        {products.map((product: Product) =>
-          isLoading ? (
-            <Skeleton
-              key={product.id}
-              animation='pulse'
-              variant='rectangular'
-              width={'100%'}
-              height={'100%'}
-              sx={{ margin: '1.5rem' }}
-            />
-          ) : (
-            <ProductCard
-              key={product.id}
-              addToCart={addToCart}
-              product={product}
-              quantity={getProductQuantity(product.id)}
-              increaseQuantity={increaseQuantity}
-              decreaseQuantity={decreaseQuantity}
-            />
-          )
-        )}
-      </Grid>
+      {Object.entries(productsGroupedByCategory).map(
+        ([category, categoryProducts]) => (
+          <Box key={category} sx={{ width: '100%', mb: 4 }}>
+            <Typography variant='h4' sx={{ mb: 3 }}>
+              {category}
+            </Typography>
+            <Grid
+              container
+              alignItems='center'
+              justifyContent='center'
+              p={5}
+              spacing={2}
+            >
+              {categoryProducts.map((product: Product) =>
+                isLoading ? (
+                  <Skeleton
+                    key={product.id}
+                    animation='pulse'
+                    variant='rectangular'
+                    width={'100%'}
+                    height={'100%'}
+                    sx={{ margin: '1.5rem' }}
+                  />
+                ) : (
+                  <ProductCard
+                    key={product.id}
+                    addToCart={addToCart}
+                    product={product}
+                    quantity={getProductQuantity(product.id)}
+                    increaseQuantity={increaseQuantity}
+                    decreaseQuantity={decreaseQuantity}
+                  />
+                )
+              )}
+            </Grid>
+          </Box>
+        )
+      )}
     </Stack>
   );
 }
